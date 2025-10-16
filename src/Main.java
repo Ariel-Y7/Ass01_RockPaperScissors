5dadcdc0ca884e9b2544de828adaf0f2b1214fe4
Ariel-Y7 <youngbal@mail.uc.edu>
2025-10-16 15:48:20 -0400

    final project

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,70 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean playAgain = true;
+
+        while (playAgain) {
+
+            String playerA = "";
+            String playerB = "";
+            boolean done = false;
+
+
+            do {
+                System.out.print("Player A, enter your move (R, P, S): ");
+                playerA = in.nextLine().toUpperCase();
+
+                if (playerA.equals("R") || playerA.equals("P") || playerA.equals("S")) {
+                    done = true;
+                } else {
+                    System.out.println("Invalid Move: " + playerA);
+                    System.out.println("Try again,");
+                }
+            } while (!done);
+
+
+            done = false;
+
+            do {
+                System.out.print("Player B, enter your move (R, P, S): ");
+                playerB = in.nextLine().toUpperCase();
+
+                if (playerB.equals("R") || playerB.equals("P") || playerB.equals("S")) {
+                    done = true;
+                } else {
+                    System.out.println("Invalid Move: " + playerB);
+                    System.out.println("Try again,");
+                }
+
+            } while (!done);
+
+            if (playerA.equals(playerB)) {
+                System.out.println("Both players chose " + playerA + " - it's a tie!");
+            } else if (playerA.equals("R") && playerB.equals("S")) {
+                System.out.println("Rock breaks Scissors - Player A wins!");
+            } else if (playerA.equals("P") && playerB.equals("R")) {
+                System.out.println("Paper covers Rock - Player A wins!");
+            } else if (playerA.equals("S") && playerB.equals("P")) {
+                System.out.println("Scissors cut Paper - Player A wins!");
+            } else if (playerB.equals("R") && playerA.equals("S")) {
+                System.out.println("Rock breaks Scissors - Player B wins!");
+            } else if (playerB.equals("P") && playerA.equals("R")) {
+                System.out.println("Paper covers Rock - Player B wins!");
+            } else if (playerB.equals("S") && playerA.equals("P")) {
+                System.out.println("Scissors cut Paper - Player B wins!");
+            }
+
+
+            System.out.print("Do you want to play again? (y/n): ");
+            String answer = in.nextLine();
+
+            if (answer.equalsIgnoreCase("n")) {
+                playAgain = false;
+                System.out.println("Thanks for playing!");
+
+            }
+        }
+    }
 }
